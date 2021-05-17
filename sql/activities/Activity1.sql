b15f436bdd8352abd0a3be9919c783781f629954
sravan-talishetti <83629289+sravan-talishetti@users.noreply.github.com>
2021-05-17 11:47:44 +0530

    sql activities

---
+++ sql/activities/Activity1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE salesman (     salesman_id int,     salesman_name varchar2(32),     salesman_city varchar2(32),     commission int );
+
+describe salesman;
+
+select * from salesman;
+
+drop table salesman;
+
+
+
