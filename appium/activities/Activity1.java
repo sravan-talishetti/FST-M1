b8bd70ae972e7e7a227641acf996c05696b3f6a4
sravan-talishetti <83629289+sravan-talishetti@users.noreply.github.com>
2021-05-17 11:35:47 +0530

    Add files via upload

---
+++ appium/activities/Activity1.java
@@ -0,0 +1,32 @@
+package activities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Activity1 {
+    @Test
+    public void calc() throws InterruptedException, IOException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "Pixel_4_API_28");
+        caps.setCapability("platformName", "Android");
+		 caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("appPackage", "com.android.calculator2");
+        caps.setCapability("appActivity", "com.android.calculator2.Calculator");
+
+        AppiumDriver<MobileElement> driver = null;
+        try {
+            driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+            System.out.println("Calculator is open");
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
