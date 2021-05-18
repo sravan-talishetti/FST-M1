5f15b2267dfc45ac591b62f8dc788470a1da5d87
sravan-talishetti <83629289+sravan-talishetti@users.noreply.github.com>
2021-05-18 19:20:46 +0530

    SQL Activities added

---
+++ sql/activities/Activity2.sql
@@ -0,0 +1,8 @@
+INSERT ALL
+    INTO salesman VALUES(5005, 'Sravan', 'London', 11)
+    INTO salesman VALUES(5006, 'Sunny', 'Paris', 14)
+    INTO salesman VALUES(5007, 'Sai', 'Rome', 13)
+    INTO salesman VALUES(5003, 'Luther', 'San Jose', 12)
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
